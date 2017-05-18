49762f90a6c0af2d1e9fdda98816cc13cbfc1896
Pedro H. B. Cavalcante <pedrinho.sjs@ufrn.edu.br>
2017-05-18 10:58:10 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>INFOrmations - Seu Canal de Informações Sobre Tecnologia</title>
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+        <link rel="stylesheet" href="css/edicaocss.css">
+         <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+    </head>
+    <body>
+        <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper">
+                        <a href="#" class="brand-logo right"><bold>INFO</bold>rmations</a>
+                    <ul id="nav-mobile" class="left hide-on-med-and-down">
+                        <li class="active"><a href="#">Início</a></li>
+                        <li><a href="#">Tecnologia</a></li>
+                        <li><a href="#">Segurança</a></li>
+                    </ul> 
+                </div>
+            </nav>
+        </div>   
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m6">
+                    <div class="card darken-1">
+                        <div class="card-content">
+                            <span class="card-title">Aqui vai o título</span>
+                            <p>Aqui vai um lorem ipsum</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">Ler Matéria</a>
+                            <a class="waves-effect waves-light btn">#tag</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m6">
+                    <div class="card darken-1">
+                        <div class="card-content">
+                            <span class="card-title">Aqui vai o título</span>
+                            <p>Aqui vai um lorem ipsum</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">Ler Matéria</a>
+                            <a class="waves-effect waves-light btn">#tag</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12 m6">
+                    <div class="card darken-1">
+                        <div class="card-content">
+                            <span class="card-title">Aqui vai o título</span>
+                            <p>Aqui vai um lorem ipsum</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">Ler Matéria</a>
+                            <a class="waves-effect waves-light btn">#tag</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m6">
+                    <div class="card darken-1">
+                        <div class="card-content">
+                            <span class="card-title">Aqui vai o título</span>
+                            <p>Aqui vai um lorem ipsum</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">Ler Matéria</a>
+                            <a class="waves-effect waves-light btn">#tag</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12 m6">
+                    <div class="card darken-1">
+                        <div class="card-content">
+                            <span class="card-title">Aqui vai o título</span>
+                            <p>Aqui vai um lorem ipsum</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">Ler Matéria</a>
+                            <a class="waves-effect waves-light btn">#tag</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m6">
+                    <div class="card darken-1">
+                        <div class="card-content">
+                            <span class="card-title">Aqui vai o título</span>
+                            <p>Aqui vai um lorem ipsum</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">Ler Matéria</a>
+                            <a class="waves-effect waves-light btn">#tag</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12 m6">
+                    <div class="card darken-1">
+                        <div class="card-content">
+                            <span class="card-title">Aqui vai o título</span>
+                            <p>Aqui vai um lorem ipsum</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">Ler Matéria</a>
+                            <a class="waves-effect waves-light btn">#tag</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m6">
+                    <div class="card darken-1">
+                        <div class="card-content">
+                            <span class="card-title">Aqui vai o título</span>
+                            <p>Aqui vai um lorem ipsum</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">Ler Matéria</a>
+                            <a class="waves-effect waves-light btn">#tag</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>      
+    </body>
+</html>
