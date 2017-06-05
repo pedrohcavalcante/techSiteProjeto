03fa67e8bd9c2ec75a311229076e486bd08b537a
Pedro H. B. Cavalcante <pedrinho.sjs@ufrn.edu.br>
2017-06-05 16:48:14 -0300

    Inserção do WISIWYG para escrever a noticia

--- admin/escreverNew.php
+++ admin/escreverNew.php
@@ -19,6 +19,7 @@ $row = $sql_query->fetch_assoc();
         <link rel="stylesheet" href="admincss/admincss.css">
          <!-- Compiled and minified JavaScript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+        <script src="ckeditor/ckeditor.js"></script>
     </head>
     <body>
         <div class="navbar-fixed">
@@ -33,14 +34,27 @@ $row = $sql_query->fetch_assoc();
             </nav>
         </div>   
     
-    <div class="row ">
-        <div class="col s3 leftRow">
-            <ul class="collection">
-                <li class="collection-item"><a href="opainel.php">Notícias</a></li>
-                <li class="collection-item active"><a href="">Escrever Notícia</a></li>
-                <!--<li class="collection-item"><a href="">Rascunho</a></li>-->
-                <li class="collection-item"><a href="cadastroconteudista.php">Cadastrar Conteudista</a></li>
-            </ul>
+        <div class="row ">
+            <div class="col s3 leftRow">
+                <ul class="collection">
+                    <li class="collection-item"><a href="opainel.php">Notícias</a></li>
+                    <li class="collection-item active"><a href="">Escrever Notícia</a></li>
+                    <!--<li class="collection-item"><a href="">Rascunho</a></li>-->
+                    <li class="collection-item"><a href="cadastroconteudista.php">Cadastrar Conteudista</a></li>
+                </ul>
+            </div>
+            <div class="col s9">
+                <form>
+                    <textarea name="editor1" id="editor1" rows="10" cols="80">
+            </textarea>
+            <script>
+                // Replace the <textarea id="editor1"> with a CKEditor
+                // instance, using default configuration.
+                CKEDITOR.replace( 'editor1' );
+            </script>
+            <br>
+            <button type='submit' class='waves-effect waves-light btn right' value='Login'>Cadastrar</button>
+                </form>
         </div>
     </body>
 </html>
